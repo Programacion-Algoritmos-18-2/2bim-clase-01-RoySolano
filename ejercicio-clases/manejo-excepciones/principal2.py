82de523ced346be6b35687c4b0b8937ca1f33f71
RoySolano <cancerlogo@hotmail.com>
2018-11-30 11:38:23 -0500

    Ejercicios de clase

---
+++ ejercicio-clases/manejo-excepciones/principal2.py
@@ -0,0 +1,20 @@
+"""Manejo de excepciones
+"""
+try:
+   numero1 = input("Ingrese un número 1: \n")
+   numero1 = int(numero1)
+   numero2 = input("Ingrese un número 2: \n")
+   numero2 = int(numero2)
+   operacion = numero1/numero2
+   print("El valor de la operación es %d" % (operacion))
+except NameError as e:
+   print("Existe un error %s" % e)
+
+except ValueError as e:
+   print("Existe un error (%s): %s" % (e.__class__, e))
+
+except ZeroDivisionError as e:
+   print("Existe un error (%s): %s" % (e.__class__, e))
+
+#except Exception as e:
+#	print("Existe un error (%s): %s" % (e.__class__, e))
